d27351331a152b308a46d1f11e5ba8e86b0865ef
Cauazzin <cauafsantos17@gmail.com>
2025-04-14 19:12:57 -0300

    Add files via upload

---
+++ Metodos-Array-Cauã/script.js
@@ -0,0 +1,35 @@
+var frutas = ["Laranja", "Banana"]
+
+/* .PUSH() ADICIONA NO FINAL */
+frutas.push("Maçã")
+
+/* .UNSHIFT() ADICIONA NO INÍCIO */
+frutas.unshift("Uva")
+
+/* .POP() REMOVE O ULTIMO */
+/* frutas.pop() */
+
+/* .SHIFT REMOVE O PRIMEIRO */
+/* frutas.shift() */
+
+/* .SPLICE(1,1) 1º PARÂMETRO É O ITEM, 2º É A QUANTIDADE */
+/* frutas.splice(1,1) */
+
+
+
+/* const user = prompt("Qual fruta deseja deletar")
+const deleteFruta = frutas.indexOf(user)
+
+frutas.splice(deleteFruta, 1)
+console.log(frutas)
+ */
+
+console.log(frutas)
+
+console.log(frutas.includes("Laranja"))
+
+
+
+
+
+
